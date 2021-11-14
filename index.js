184ee819535cdebc4235ca2e319b285945d12299
Mclovin <felipe.ponce2@mail.udp.cl>
2021-11-14 11:35:58 -0300

    Update index.js

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const port = 3000;
 
 app.get('/', (req, res) =>{
     res.send("Holaaaa");
-}
-	app.listen(process.env.PORT || 3000, () => {
+})
+app.listen(process.env.PORT || 3000, () => {
     console.log('Funcionando');
-});)
+});
