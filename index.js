0e70749e63210391a7a301290ceae4f695678419
Felipe Ignacio Ponce Canales <felipe.ponce2@mail.udp.cl>
2021-11-17 01:26:44 +0000

    avance

--- index.js
+++ index.js
@@ -2,6 +2,9 @@ const express = require('express');
 const fire = require('firebase/app')
 const initializeApp = fire.initializeApp;
 const app2 = express();
+const cors = require('cors');
+const mongoose = require('mongoose');
+const Router = require('./routes.js')
 const firebaseConfig = {
   apiKey: "AIzaSyDvzlzuF2bM37DAST0fD-1pciXa2HRUPZ4",
   authDomain: "lab-simetrico.firebaseapp.com",
@@ -14,10 +17,27 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
+//Init MongoDB
+main().catch(err => console.log(err));
+async function main() {
+  await mongoose.connect("mongodb+srv://dbMclovin:Javi2602@ggtienda.5rbjo.mongodb.net/myFirstDatabase?retryWrites=true&w=majority");
 
-
+}
+const db = mongoose.connection;
+db.on("error", console.error.bind(console, "connection error: "));
+db.once("open", function () {
+  console.log("Connected successfully");
+});
+app2.use(Router);
 app2.get('/', (req, res) =>{
     res.send("Holaaaa");
+})
+app2.use(express.urlencoded({ extended: false }));
+app2.use(express.json());
+app2.use(cors());
+app2.post('/agregar', (req, res)=>{
+  const {ar1, ar2, ar3} = req.body;
+  console.log(req.body);
 })
 app2.listen(process.env.PORT || 3000, () => {
     console.log('Funcionando');
